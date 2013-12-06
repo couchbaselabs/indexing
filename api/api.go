464e9908d138de40fbe1617320f26570ddc6ce6a
deepkaran <deepkaran.salooja@gmail.com>
2013-12-07 00:36:54 +0530

    Change formatting to gofmt

--- api/api.go
+++ api/api.go
@@ -5,151 +5,158 @@ package api
 
 // Known index types
 type IndexType string
+
 const (
-    View    IndexType = "view"
-    Llrb              = "llrb"
-    LevelDB           = "leveldb"
-    RocksDB           = "rocksdb"
-    HyperDB           = "hyperdb"
-    RBTree            = "rbtree"
-    CBTree            = "cbtree"
+	View    IndexType = "view"
+	Llrb              = "llrb"
+	LevelDB           = "leveldb"
+	RocksDB           = "rocksdb"
+	HyperDB           = "hyperdb"
+	RBTree            = "rbtree"
+	CBTree            = "cbtree"
 )
 
 // Inclusion, controls how the boundaries values of a range are treated
 type Inclusion string
+
 const (
-    Neither Inclusion = "none"
-    Left              = "left"
-    Right             = "right"
-    Both              = "both"
+	Neither Inclusion = "none"
+	Left              = "left"
+	Right             = "right"
+	Both              = "both"
 )
 
 // Uniqueness, characterizes if the algorithm demands unique keys
 type Uniqueness bool
+
 const (
-    Unique    Uniqueness = true
-    NonUnique            = false
+	Unique    Uniqueness = true
+	NonUnique            = false
 )
 
 // SortOrder characterizes if the algorithm emits keys in a predictable order
 type SortOrder string
+
 const (
-    Unsorted SortOrder = "none"
-    Asc                = "asc"
-    Desc               = "desc"
+	Unsorted SortOrder = "none"
+	Asc                = "asc"
+	Desc               = "desc"
 )
 
 // Expression to be applied on the document to get the secondary key.
 type ExprType string
-const(
-    Simple     string = "simple"
-    JavaScript        = "javascript"
-    N1QL              = "n1ql"
+
+const (
+	Simple     string = "simple"
+	JavaScript        = "javascript"
+	N1QL              = "n1ql"
 )
 
 // Every index ever created and maintained by this package will have an
 // associated index-info structure.
 type IndexInfo struct {
-    Name       string    // Name of the index
-    Uuid       string    // unique id for every new index created.
-    Using      IndexType // indexing algorithm to use / used.
-    CreateStmt string    // in case the index was created by N1QL DDL
-    Bucket     string    // bucket name, for which the index is created.
-    IsPrimary  bool      // true/false based on index
-    Exprtype   ExprType  // type of `Expression`
-    Expression string    // expression content, check out ExprType
-    Index      Finder    // instance of index algorithm.
+	Name       string    // Name of the index
+	Uuid       string    // unique id for every new index created.
+	Using      IndexType // indexing algorithm to use / used.
+	CreateStmt string    // in case the index was created by N1QL DDL
+	Bucket     string    // bucket name, for which the index is created.
+	IsPrimary  bool      // true/false based on index
+	Exprtype   ExprType  // type of `Expression`
+	Expression string    // expression content, check out ExprType
+	Index      Finder    // instance of index algorithm.
 }
 
 // Accuracy characterizes if the results of the index is subject to probabilistic errors.
 // When an algorithm that is not Perfect is used, the caller must verify the results.
 type Accuracy float64
+
 const (
-    Useless Accuracy = 0.0
-    Perfect          = 1.0
+	Useless Accuracy = 0.0
+	Perfect          = 1.0
 )
 
 // Complexity characterizes space and time characteristics of the algorithm
 type Complexity int
+
 const (
-    O1     Complexity = iota
-    Ologm
-    Ologn
-    Om
-    Omlogm
-    Omlogn
-    On
-    Onlogn
-    Om2
-    On2
-    Ounknown
+	O1 Complexity = iota
+	Ologm
+	Ologn
+	Om
+	Omlogm
+	Omlogn
+	On
+	Onlogn
+	Om2
+	On2
+	Ounknown
 )
 
 // TraitInfo is collection of traits of an algorithm. One can query the traits of an
 // entire indexing algorithm, or traits of a specific operation. May change soon.
 type TraitInfo struct {
-    Unique     Uniqueness
-    Order      SortOrder
-    Accuracy   Accuracy
-    AvgTime    Complexity
-    AvgSpace   Complexity
-    WorstTime  Complexity
-    WorstSpace Complexity
+	Unique     Uniqueness
+	Order      SortOrder
+	Accuracy   Accuracy
+	AvgTime    Complexity
+	AvgSpace   Complexity
+	WorstTime  Complexity
+	WorstSpace Complexity
 }
 
 // Indexer is the interface into the index engine
 type IndexManager interface {
-    // Create builds an instance of index
-    Create(indexInfo IndexInfo) (string, IndexInfo, error)
+	// Create builds an instance of index
+	Create(indexInfo IndexInfo) (string, IndexInfo, error)
 
-    // Drop kills an instance of an index
-    Drop(uuid string) (string, error)
+	// Drop kills an instance of an index
+	Drop(uuid string) (string, error)
 
-    // If `ServerUuid` is not nil, then check to see if the local ServerUUID
-    // matches it. A match means client already has latest server
-    // information and index data is not sent. A zero value makes server send
-    // the latest index data unconditionally.
-    //
-    // Returned list IndexInfo won't contain the index instance.
-    List(ServerUuid string) (string, []IndexInfo, error)
+	// If `ServerUuid` is not nil, then check to see if the local ServerUUID
+	// matches it. A match means client already has latest server
+	// information and index data is not sent. A zero value makes server send
+	// the latest index data unconditionally.
+	//
+	// Returned list IndexInfo won't contain the index instance.
+	List(ServerUuid string) (string, []IndexInfo, error)
 
-    // Gets a specific instance
-    Index(uuid string) (IndexInfo, error)
+	// Gets a specific instance
+	Index(uuid string) (IndexInfo, error)
 
-    // Get Uuid
-    GetUuid() string
+	// Get Uuid
+	GetUuid() string
 }
 
 type Key interface {
-    Bytes() []byte        // content of key as byte representation
-    Less(than Key) bool   // compare whether `this` key is less than `than` key
-    Compare(than Key) Ord // compare whether `this` key is less than `than` key
+	Bytes() []byte        // content of key as byte representation
+	Less(than Key) bool   // compare whether `this` key is less than `than` key
+	Compare(than Key) Ord // compare whether `this` key is less than `than` key
 }
 
-type Value interface{
-    Bytes() []byte // content of value, typically document-id
+type Value interface {
+	Bytes() []byte // content of value, typically document-id
 }
 
 type KV [2]interface{} // [Key, Value]
 
 // Algorithm is the basic capability of any index algorithm
 type Finder interface {
-    Name() string
-    Purge()
-    Trait(operator interface{}) TraitInfo
+	Name() string
+	Purge()
+	Trait(operator interface{}) TraitInfo
 }
 
 // Counter is a class of algorithms that return total node count efficiently
 type Counter interface {
-    Finder
-    CountTotal() (uint64, error)
+	Finder
+	CountTotal() (uint64, error)
 }
 
 // Exister is a class of algorithms that allow testing if a key exists in the
 // index
 type Exister interface {
-    Finder
-    Exists(key Key) bool // TODO: Should we have the `error` part of return ?
+	Finder
+	Exists(key Key) bool // TODO: Should we have the `error` part of return ?
 }
 
 // Looker is a class of algorithms that allow looking up a key in an index.
@@ -159,24 +166,24 @@ type Exister interface {
 // TODO: Define the semantics of buffer size of channels that are returned by
 // the following method receiver.
 type Looker interface {
-    Exister
-    Lookup(key Key) (chan Value, chan error)
-    KeySet() (chan Key, chan error)
-    ValueSet() (chan Value, chan error)
-    KVSet() (chan KV, chan error)
+	Exister
+	Lookup(key Key) (chan Value, chan error)
+	KeySet() (chan Key, chan error)
+	ValueSet() (chan Value, chan error)
+	KVSet() (chan KV, chan error)
 }
 
 // Ranger is a class of algorithms that can extract a range of keys from the
 // index.
 type Ranger interface {
-    Looker
-    KeyRange(low, high Key, inclusion Inclusion) (chan Key, chan error, SortOrder)
-    ValueRange(low, high Key, inclusion Inclusion) (chan Value, chan error, SortOrder)
-    KVRange(low Key, high Key, inclusion Inclusion) (chan KV, chan error, SortOrder)
+	Looker
+	KeyRange(low, high Key, inclusion Inclusion) (chan Key, chan error, SortOrder)
+	ValueRange(low, high Key, inclusion Inclusion) (chan Value, chan error, SortOrder)
+	KVRange(low Key, high Key, inclusion Inclusion) (chan KV, chan error, SortOrder)
 }
 
 // RangeCounter is a class of algorithms that can count a range efficiently
 type RangeCounter interface {
-    Finder
-    CountRange(low Key, high Key, inclusion Inclusion) (uint64, error)
+	Finder
+	CountRange(low Key, high Key, inclusion Inclusion) (uint64, error)
 }
--- server/indexer_test.go
+++ server/indexer_test.go
@@ -1,137 +1,137 @@
 package server
 
-import(
-    "testing"
-    "os"
-    "path/filepath"
-    "github.com/couchbaselabs/indexing/api"
-    "github.com/couchbaselabs/indexing/llrb"
+import (
+	"github.com/couchbaselabs/indexing/api"
+	"github.com/couchbaselabs/indexing/llrb"
+	"os"
+	"path/filepath"
+	"testing"
 )
 
 func TestTrycreate(t *testing.T) {
-    datadir := "./"
-    os.Remove(filepath.Join(datadir, CATALOGFILE))
+	datadir := "./"
+	os.Remove(filepath.Join(datadir, CATALOGFILE))
 
-    indexer := &IndexCatalog{
-        DefaultLimit: DEFAULT_LIMIT,
-        Datadir: datadir,
-        File: filepath.Join(datadir, CATALOGFILE),
-    }
-    if err := indexer.tryCreate(); err != nil {
-        t.Error("tryCreate failed:", err)
-    }
-    if _, err := os.Stat(indexer.File); err != nil {
-        t.Error("tryCreate did not create catalog file:", err)
-    }
-    indexer.Purge()
+	indexer := &IndexCatalog{
+		DefaultLimit: DEFAULT_LIMIT,
+		Datadir:      datadir,
+		File:         filepath.Join(datadir, CATALOGFILE),
+	}
+	if err := indexer.tryCreate(); err != nil {
+		t.Error("tryCreate failed:", err)
+	}
+	if _, err := os.Stat(indexer.File); err != nil {
+		t.Error("tryCreate did not create catalog file:", err)
+	}
+	indexer.Purge()
 }
 
 func TestCodec(t *testing.T) {
-    var indexer *IndexCatalog
-    var err error
-    datadir := "./"
-    os.Remove(filepath.Join(datadir, CATALOGFILE))
+	var indexer *IndexCatalog
+	var err error
+	datadir := "./"
+	os.Remove(filepath.Join(datadir, CATALOGFILE))
 
-    if indexer, err = NewIndexCatalog(datadir); err != nil {
-        t.Error("Cannot create index catalog file")
-    }
-    if len(indexer.Indexes) != 0 {
-        t.Error("Indexes must be zero")
-    }
-    indexer.saveCatalog()
-    if len(indexer.Indexes) != 0 {
-        t.Error("Indexes must be zero")
-    }
-    indexer.loadCatalog()
-    indexer.Purge()
+	if indexer, err = NewIndexCatalog(datadir); err != nil {
+		t.Error("Cannot create index catalog file")
+	}
+	if len(indexer.Indexes) != 0 {
+		t.Error("Indexes must be zero")
+	}
+	indexer.saveCatalog()
+	if len(indexer.Indexes) != 0 {
+		t.Error("Indexes must be zero")
+	}
+	indexer.loadCatalog()
+	indexer.Purge()
 }
 
 func TestCatalog(t *testing.T) {
-    var indexer, indexer_ *IndexCatalog
-    var indexinfo api.IndexInfo
-    var servUuid  string
-    var err       error
+	var indexer, indexer_ *IndexCatalog
+	var indexinfo api.IndexInfo
+	var servUuid string
+	var err error
 
-    datadir := "./"
-    os.Remove(filepath.Join(datadir, CATALOGFILE))
-    if indexer_, err := NewIndexCatalog(datadir); err != nil {
-        t.Fatal("Cannot open index catalog file")
-    } else {
-        indexer = indexer_
-    }
+	datadir := "./"
+	os.Remove(filepath.Join(datadir, CATALOGFILE))
+	if indexer_, err := NewIndexCatalog(datadir); err != nil {
+		t.Fatal("Cannot open index catalog file")
+	} else {
+		indexer = indexer_
+	}
 
-    indexinfo = api.IndexInfo{
-        Name:       "test",
-        Using:      api.Llrb,
-        CreateStmt: `CREATE INDEX emailidx ON users (age+10)`,
-        Bucket:     "users",
-        IsPrimary:  false,
-        Exprtype:   "simple",
-        Expression: "",
-    }
+	indexinfo = api.IndexInfo{
+		Name:       "test",
+		Using:      api.Llrb,
+		CreateStmt: `CREATE INDEX emailidx ON users (age+10)`,
+		Bucket:     "users",
+		IsPrimary:  false,
+		Exprtype:   "simple",
+		Expression: "",
+	}
 
-    // Test Create()
-    if servUuid, indexinfo, err = indexer.Create(indexinfo); err != nil {
-        t.Error("Cannot create index", indexinfo)
-    } else if servUuid == "" {
-        t.Error("server uuid is null")
-    } else if indexinfo.Index == nil {
-        t.Error("index.Index is nil after create index")
-    } else if indexinfo.Uuid == "" {
-        t.Error("index uuid is null")
-    } else if len(indexer.Indexes) != 1 {
-        t.Error("Indexes must have one index")
-    }
-    // Reload the indexes from catalog
-    if indexer_, err = NewIndexCatalog(datadir); err != nil {
-        panic(err)
-        t.Fatal("Cannot re-open index catalog file", err)
-    } else if len(indexer.Indexes) != 1 {
-        t.Error("Indexes must have one index")
-    } else {
-        indexinfo := indexer_.Indexes[indexinfo.Uuid]
-        if _, ok := indexinfo.Index.(*llrb.IndexLLRB); ok == false {
-            t.Error("Cannot load back the index")
-        } else {
-            indexer = indexer_
-        }
-    }
+	// Test Create()
+	if servUuid, indexinfo, err = indexer.Create(indexinfo); err != nil {
+		t.Error("Cannot create index", indexinfo)
+	} else if servUuid == "" {
+		t.Error("server uuid is null")
+	} else if indexinfo.Index == nil {
+		t.Error("index.Index is nil after create index")
+	} else if indexinfo.Uuid == "" {
+		t.Error("index uuid is null")
+	} else if len(indexer.Indexes) != 1 {
+		t.Error("Indexes must have one index")
+	}
+	// Reload the indexes from catalog
+	if indexer_, err = NewIndexCatalog(datadir); err != nil {
+		panic(err)
+		t.Fatal("Cannot re-open index catalog file", err)
+	} else if len(indexer.Indexes) != 1 {
+		t.Error("Indexes must have one index")
+	} else {
+		indexinfo := indexer_.Indexes[indexinfo.Uuid]
+		if _, ok := indexinfo.Index.(*llrb.IndexLLRB); ok == false {
+			t.Error("Cannot load back the index")
+		} else {
+			indexer = indexer_
+		}
+	}
 
-    // Test Index()
-    if indexinfo, err = indexer.Index(indexinfo.Uuid); err != nil {
-        t.Error("Index() returned", err)
-    } else if indexinfo.Name != indexinfo.Name {
-        t.Error("Index() api fails", indexinfo)
-    }
+	// Test Index()
+	if indexinfo, err = indexer.Index(indexinfo.Uuid); err != nil {
+		t.Error("Index() returned", err)
+	} else if indexinfo.Name != indexinfo.Name {
+		t.Error("Index() api fails", indexinfo)
+	}
 
-    // Test List()
-    if _, ls, err := indexer.List(""); err != nil {
-        t.Error(err)
-    } else {
-        if len(ls) != 1 {
-            t.Error("List returns", len(ls))
-        }
-        if ls[0].CreateStmt != indexinfo.CreateStmt {
-            t.Error("List returns create statement", ls[0].CreateStmt)
-        }
-    }
+	// Test List()
+	if _, ls, err := indexer.List(""); err != nil {
+		t.Error(err)
+	} else {
+		if len(ls) != 1 {
+			t.Error("List returns", len(ls))
+		}
+		if ls[0].CreateStmt != indexinfo.CreateStmt {
+			t.Error("List returns create statement", ls[0].CreateStmt)
+		}
+	}
 
-    // Test Drop()
-    if _, err := indexer.Drop(indexinfo.Uuid); err != nil {
-        t.Error("Drop() returned", err)
-    }
-    // Reload the indexes from catalog
-    if indexer_, err := NewIndexCatalog(datadir); err != nil {
-        t.Fatal("Cannot re-open index catalog file")
-    } else if len(indexer.Indexes) != 0 {
-        t.Error("Indexes must have one index")
-    } else {
-        indexer = indexer_
-    }
+	// Test Drop()
+	if _, err := indexer.Drop(indexinfo.Uuid); err != nil {
+		t.Error("Drop() returned", err)
+	}
+	// Reload the indexes from catalog
+	if indexer_, err := NewIndexCatalog(datadir); err != nil {
+		t.Fatal("Cannot re-open index catalog file")
+	} else if len(indexer.Indexes) != 0 {
+		t.Error("Indexes must have one index")
+	} else {
+		indexer = indexer_
+	}
 
-    // Test Purge()
-    indexer.Purge()
-    if _, err := os.Stat(indexer.File); err == nil {
-        t.Error("Purge did not delete the catalog file")
-    }
+	// Test Purge()
+	indexer.Purge()
+	if _, err := os.Stat(indexer.File); err == nil {
+		t.Error("Purge did not delete the catalog file")
+	}
 }
