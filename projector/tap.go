5e4368088294e353dc573e9a7f52e0eca1b4661b
prataprc <prataprc@gmail.com>
2014-01-04 16:02:23 +0530

    UprStreams() are opened inside a goroutine.
    - This may not be a proper fix. the problem scenario is as follows,
    
        when application first call UprStream() to open more than one stream,
        supplying an stream-event channel to receive mutation,
        MUTATIONS received from previously opened stream can block on
        application's stream-event channel and not receive response for the last
        call to UprStream(). Leading to a deadlock.
    
      Right now, we are avoiding this by doing UprStream in a go-routine.

--- projector/tap.go
+++ projector/tap.go
@@ -1,98 +1,98 @@
 package main
 
 import (
-    "github.com/couchbaselabs/go-couchbase"
-    mc "github.com/dustin/gomemcached/client"
-    "github.com/prataprc/goupr"
-    "log"
+	"github.com/couchbaselabs/go-couchbase"
+	mc "github.com/dustin/gomemcached/client"
+	"github.com/prataprc/goupr"
+	"log"
 )
 
 type TapStreams struct {
-    client  *couchbase.Client
-    pool    *couchbase.Pool
-    buckets map[string]*couchbase.Bucket // [bucketname]*couchbase.Bucket
-    feeds   map[string]*couchbase.TapFeed // [bucketname]*couchbase.TapFeed
-    eventch chan *goupr.StreamEvent
+	client  *couchbase.Client
+	pool    *couchbase.Pool
+	buckets map[string]*couchbase.Bucket  // [bucketname]*couchbase.Bucket
+	feeds   map[string]*couchbase.TapFeed // [bucketname]*couchbase.TapFeed
+	eventch chan *goupr.StreamEvent
 }
 
 var tapop2type = map[mc.TapOpcode]string{
-    mc.TapMutation: "INSERT",
-    mc.TapDeletion: "DELETE",
+	mc.TapMutation: "INSERT",
+	mc.TapDeletion: "DELETE",
 }
 
 func NewTapStreams(c *couchbase.Client, p *couchbase.Pool,
-    eventch chan *goupr.StreamEvent) *TapStreams {
+	eventch chan *goupr.StreamEvent) *TapStreams {
 
-    return &TapStreams{
-        client:     c,
-        pool:       p,
-        buckets:    make(map[string]*couchbase.Bucket),
-        feeds:      make(map[string]*couchbase.TapFeed),
-        eventch:    eventch,
-    }
+	return &TapStreams{
+		client:  c,
+		pool:    p,
+		buckets: make(map[string]*couchbase.Bucket),
+		feeds:   make(map[string]*couchbase.TapFeed),
+		eventch: eventch,
+	}
 }
 
 func (streams *TapStreams) OpenStreams(buckets []string) {
-    var bucket *couchbase.Bucket
-    var pool couchbase.Pool
-    var err error
+	var bucket *couchbase.Bucket
+	var pool couchbase.Pool
+	var err error
 
-    pool, err = streams.client.GetPool("default")
-    if err != nil {
-        log.Println("ERROR: Unable to refresh the pool `default`")
-        return
-    }
-    streams.pool = &pool
-    for _, bname := range buckets {
-        if bucket, err = streams.pool.GetBucket(bname); err != nil {
-            log.Println("ERROR: failed to get bucket", bname, "err:", err)
-            break
-        }
-        streams.buckets[bname] = bucket
-        args := mc.TapArguments{
-            Dump:       false,
-            SupportAck: false,
-            KeysOnly:   false,
-            Checkpoint: true,
-            ClientName: "",
-        }
-        if tapfeed, err := bucket.StartTapFeed(&args); err == nil {
-            streams.feeds[bname] = tapfeed
-            go runFeed(streams, bname, tapfeed)
-        } else {
-            log.Println("ERROR: failed to get feed for bucket", bname, "err:", err)
-            break
-        }
-    }
-    return
+	pool, err = streams.client.GetPool("default")
+	if err != nil {
+		log.Println("ERROR: Unable to refresh the pool `default`")
+		return
+	}
+	streams.pool = &pool
+	for _, bname := range buckets {
+		if bucket, err = streams.pool.GetBucket(bname); err != nil {
+			log.Println("ERROR: failed to get bucket", bname, "err:", err)
+			break
+		}
+		streams.buckets[bname] = bucket
+		args := mc.TapArguments{
+			Dump:       false,
+			SupportAck: false,
+			KeysOnly:   false,
+			Checkpoint: true,
+			ClientName: "",
+		}
+		if tapfeed, err := bucket.StartTapFeed(&args); err == nil {
+			streams.feeds[bname] = tapfeed
+			go runFeed(streams, bname, tapfeed)
+		} else {
+			log.Println("ERROR: failed to get feed for bucket", bname, "err:", err)
+			break
+		}
+	}
+	return
 }
 
 func runFeed(streams *TapStreams, b string, tapfeed *couchbase.TapFeed) {
-    log.Println("feed for bucket", b, "...")
-    for {
-        if event, ok := <-tapfeed.C; ok {
-            op := event.Opcode
-            if op == mc.TapMutation || op == mc.TapDeletion {
-                streams.eventch <- &goupr.StreamEvent{
-                    Bucket: b,
-                    Opstr:  tapop2type[op],
-                    Key:    event.Key,
-                    Value:  event.Value,
-                }
-            }
-        } else {
-            log.Println("Closing tap feed for", b)
-            break
-        }
-    }
+	log.Println("feed for bucket", b, "...")
+	for {
+		if event, ok := <-tapfeed.C; ok {
+			op := event.Opcode
+			if op == mc.TapMutation || op == mc.TapDeletion {
+				streams.eventch <- &goupr.StreamEvent{
+					Bucket: b,
+					Opstr:  tapop2type[op],
+					Key:    event.Key,
+					Value:  event.Value,
+				}
+			}
+		} else {
+			log.Println("Closing tap feed for", b)
+			break
+		}
+	}
 }
 
 func (streams *TapStreams) CloseStreams() {
-    log.Printf("Closing %v streams", len(streams.feeds))
-    for _, bucket := range streams.buckets {
-        bucket.Close()
-    }
-    for _, tapfeed := range streams.feeds {
-        tapfeed.Close()
-    }
+	log.Printf("Closing %v streams", len(streams.feeds))
+	for _, bucket := range streams.buckets {
+		bucket.Close()
+	}
+	for _, tapfeed := range streams.feeds {
+		tapfeed.Close()
+	}
 }
--- projector/upr.go
+++ projector/upr.go
@@ -1,111 +1,113 @@
 package main
 
 import (
-    "fmt"
-    "github.com/couchbaselabs/go-couchbase"
-    "github.com/prataprc/goupr"
-    "log"
-    "strings"
+	"fmt"
+	"github.com/couchbaselabs/go-couchbase"
+	"github.com/prataprc/goupr"
+	"log"
+	"strings"
 )
 
 type UprStreams struct {
-    client  *couchbase.Client
-    pool    *couchbase.Pool
-    buckets map[string]*couchbase.Bucket // [bucketname]*couchbase.Bucket
-    // [bucketname]{hostname : *goupr.Client}
-    clients map[string]map[string]*goupr.Client
-    eventch chan *goupr.StreamEvent
+	client  *couchbase.Client
+	pool    *couchbase.Pool
+	buckets map[string]*couchbase.Bucket // [bucketname]*couchbase.Bucket
+	// [bucketname]{hostname : *goupr.Client}
+	clients map[string]map[string]*goupr.Client
+	eventch chan *goupr.StreamEvent
 }
 
-func NewUprStreams(c *couchbase.Client, p *couchbase.Pool, 
-    eventch chan *goupr.StreamEvent) *UprStreams {
+func NewUprStreams(c *couchbase.Client, p *couchbase.Pool,
+	eventch chan *goupr.StreamEvent) *UprStreams {
 
-    return &UprStreams{
-        client:  c,
-        pool:    p,
-        buckets: make(map[string]*couchbase.Bucket),
-        clients: make(map[string]map[string]*goupr.Client),
-        eventch: eventch,
-    }
+	return &UprStreams{
+		client:  c,
+		pool:    p,
+		buckets: make(map[string]*couchbase.Bucket),
+		clients: make(map[string]map[string]*goupr.Client),
+		eventch: eventch,
+	}
 }
 
 func (streams *UprStreams) OpenStreams(buckets []string) {
-    var err error
-    var pool couchbase.Pool
+	var err error
+	var pool couchbase.Pool
 
-    pool, err = streams.client.GetPool("default")
-    if err != nil {
-        log.Println("ERROR: Unable to refresh the pool `default`")
-        return
-    }
-    streams.pool = &pool
-    for _, bname := range buckets {
-        b, _ := streams.pool.GetBucket(bname)
-        streams.buckets[bname] = b
-        if streams.clients[bname], err = streams.connectNodes(b); err != nil {
-            log.Println(err)
-        } else {
-            vbmaps := b.VBSMJson.VBucketMap
-            opaqCount := uint32(1)
-            for vbucket, _ := range vbmaps {
-                openFeed(b, streams.clients[bname], uint16(vbucket), opaqCount)
-                opaqCount += 1
-                //break // TODO: REmove this
-            }
-        }
-        //break // TODO: REmove this
-    }
-    return
+	// Refresh the pool to get any new buckets created on the server.
+	pool, err = streams.client.GetPool("default")
+	if err != nil {
+		log.Println("ERROR: Unable to refresh the pool `default`")
+		return
+	}
+	streams.pool = &pool
+
+	for _, bname := range buckets {
+		b, _ := streams.pool.GetBucket(bname)
+		streams.buckets[bname] = b
+		if streams.clients[bname], err = streams.connectNodes(b); err != nil {
+			log.Println(err)
+		} else {
+			vbmaps := b.VBSMJson.VBucketMap
+			opaqCount := uint32(1)
+			for vbucket, _ := range vbmaps {
+				openFeed(b, streams.clients[bname], uint16(vbucket), opaqCount)
+				opaqCount += 1
+				//break // TODO: Remove this
+			}
+		}
+		//break // TODO: Remove this
+	}
+	return
 }
 
 func (streams *UprStreams) CloseStreams() {
-    log.Println("Closing feeds ...")
-    for _, clientmap := range streams.clients {
-        for _, client := range clientmap {
-            client.Close()
-        }
-    }
-    for _, bucket := range streams.buckets {
-        bucket.Close()
-    }
+	log.Println("Closing feeds ...")
+	for _, clientmap := range streams.clients {
+		for _, client := range clientmap {
+			client.Close()
+		}
+	}
+	for _, bucket := range streams.buckets {
+		bucket.Close()
+	}
 }
 
 func (streams *UprStreams) connectNodes(
-    bucket *couchbase.Bucket) (map[string]*goupr.Client, error) {
+	bucket *couchbase.Bucket) (map[string]*goupr.Client, error) {
 
-    servers := bucket.VBSMJson.ServerList
-    nodes := make(map[string]*goupr.Client)
-    for _, hostname := range servers {
-        client := goupr.NewClient(bucket, streams.eventch)
-        name := strings.Join([]string{"indexer", bucket.Name, hostname}, "/")
-        if err := client.Connect(hostname, name, false); err != nil {
-            return nil, fmt.Errorf("Not able to connect with %v: %v", name, err)
-        } else {
-            log.Printf("Connected to %v as %v", hostname, name)
-        }
-        client.AutoRestart(true)
-        nodes[hostname] = client
-    }
-    return nodes, nil
+	servers := bucket.VBSMJson.ServerList
+	nodes := make(map[string]*goupr.Client)
+	for _, hostname := range servers {
+		client := goupr.NewClient(bucket, streams.eventch)
+		name := strings.Join([]string{"indexer", bucket.Name, hostname}, "/")
+		if err := client.Connect(hostname, name, false); err != nil {
+			return nil, fmt.Errorf("Not able to connect with %v: %v", name, err)
+		} else {
+			log.Printf("Connected to %v as %v", hostname, name)
+		}
+		client.AutoRestart(true)
+		nodes[hostname] = client
+	}
+	return nodes, nil
 }
 
 func openFeed(b *couchbase.Bucket, nodes map[string]*goupr.Client,
-    vb uint16, opaque uint32) {
+	vb uint16, opaque uint32) {
 
-    servers, vbmaps := b.VBSMJson.ServerList, b.VBSMJson.VBucketMap
+	servers, vbmaps := b.VBSMJson.ServerList, b.VBSMJson.VBucketMap
 
-    // vbs_highseq_no := b.GetStats("vbucket-seqno")
-    // key := fmt.Sprintf("vb_%v_high_seqno", vb)
-    node := servers[vbmaps[int(vb)][0]]
-    client := nodes[node]
-    req := goupr.NewRequest(0, 0, opaque, vb)
-    vuuid, flags := uint64(0), uint32(0)
-    high, start, end := uint64(0), uint64(0), uint64(0xFFFFFFFFFFFFFFF)
-    stream, _, err := client.UprStream(req, flags, start, end, vuuid, high)
-    if err != nil {
-        log.Println("Error opening feed for vbucket", vb, err)
-    } else {
-        log.Println("Opening feed for vbucket", vb)
-        stream.AutoRestart(true)
-    }
+	// vbs_highseq_no := b.GetStats("vbucket-seqno")
+	// key := fmt.Sprintf("vb_%v_high_seqno", vb)
+	node := servers[vbmaps[int(vb)][0]]
+	client := nodes[node]
+	req := goupr.NewRequest(0, 0, opaque, vb)
+	vuuid, flags := uint64(0), uint32(0)
+	high, start, end := uint64(0), uint64(0), uint64(0xFFFFFFFFFFFFFFF)
+	stream, _, err := client.UprStream(req, flags, start, end, vuuid, high)
+	if err != nil {
+		log.Println("Error opening feed for vbucket", vb, err)
+	} else {
+		log.Printf("Opened feed for %v:%v", b.Name, vb)
+		stream.AutoRestart(true)
+	}
 }
