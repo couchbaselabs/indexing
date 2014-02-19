890c5a78c6d6b5e33c36d7eac1429919572c4d77
deepkaran <deepkaran.salooja@gmail.com>
2014-02-19 11:32:47 +0530

    Add numWorkers and batchSize for fake projector

--- indexer/examples/fake_projector/fake_projector.go
+++ indexer/examples/fake_projector/fake_projector.go
@@ -12,26 +12,17 @@ import (
 
 func main() {
 
-	go pumpMutationSet(1, 5000000)
-	go pumpMutationSet(5000001, 10000000)
-	go pumpMutationSet(10000001, 15000000)
-	go pumpMutationSet(15000001, 20000000)
-	go pumpMutationSet(20000001, 25000000)
-	go pumpMutationSet(25000001, 30000000)
-	go pumpMutationSet(30000001, 35000000)
-	go pumpMutationSet(35000001, 40000000)
-	go pumpMutationSet(40000001, 45000000)
-	go pumpMutationSet(45000001, 50000000)
-	go pumpMutationSet(50000001, 55000000)
-	go pumpMutationSet(55000001, 60000000)
-	go pumpMutationSet(60000001, 65000000)
-	go pumpMutationSet(65000001, 70000000)
-	go pumpMutationSet(70000001, 75000000)
-	go pumpMutationSet(75000001, 80000000)
-	go pumpMutationSet(80000001, 85000000)
-	go pumpMutationSet(85000001, 90000000)
-	go pumpMutationSet(90000001, 95000000)
-	pumpMutationSet(95000001, 100000000)
+	batchSize := 20000000
+	numWorkers := 20
+
+	for i := 0; i < numWorkers; i++ {
+		if i < numWorkers-1 {
+			go pumpMutationSet(i*batchSize+1, (i+1)*batchSize)
+		} else {
+			pumpMutationSet(i*batchSize+1, (i+1)*batchSize)
+		}
+	}
+
 }
 
 func pumpMutationSet(startNum int, endNum int) {
